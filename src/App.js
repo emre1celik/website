1bcbfbf250018b21d0547cfd4bfd98d060f302de
emre1celik <emre1celik@gmail.com>
2025-09-23 17:27:10 +0200

    NotFound

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./Landing";
 import Register from "./Register";
+import NotFound from "./NotFound";
 
 function App() {
   return (
