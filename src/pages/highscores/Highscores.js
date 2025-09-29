d5ebe8386b9dadcfc7686ae0d01cca83094c1138
emre1celik <emre1celik@gmail.com>
2025-09-29 16:45:27 +0200

    X

--- src/pages/highscores/Highscores.js
+++ src/pages/highscores/Highscores.js
@@ -11,15 +11,23 @@ import Navigation from "../../components/navigation/Navigation";
 import Footer from "../../components/footer/Footer";
 import { Helmet } from "react-helmet";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faCrown,
-  faMedal,
-  faAward,
-  faSpinner,
-} from "@fortawesome/free-solid-svg-icons";
-import BerserkerIcon from "../../assets/images/classes/Berserker_class_icon_White.png";
-import DeadeyeIcon from "../../assets/images/classes/Deadeye_class_icon_White.png";
-import DefaultIcon from "../../assets/images/classes/Hashashin_class_icon_White.png";
+import { faCrown, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import DwIcon from "../../assets/images/classes/dw.png";
+import DkIcon from "../../assets/images/classes/dk.png";
+import ElfIcon from "../../assets/images/classes/elf.png";
+import MgIcon from "../../assets/images/classes/mg.png";
+import DlIcon from "../../assets/images/classes/dl.png";
+import SumIcon from "../../assets/images/classes/sum.png";
+import RfIcon from "../../assets/images/classes/rf.png";
+import GlIcon from "../../assets/images/classes/gl.png";
+import RwIcon from "../../assets/images/classes/rw.png";
+import SlIcon from "../../assets/images/classes/sl.png";
+import GcIcon from "../../assets/images/classes/gc.png";
+import LwIcon from "../../assets/images/classes/lw.png";
+import MaIcon from "../../assets/images/classes/ma.png";
+import IkIcon from "../../assets/images/classes/ik.png";
+
+import DefaultIcon from "../../assets/images/classes/default.png";
 
 function Highscores({ user }) {
   const [selectedClass, setSelectedClass] = useState("all");
@@ -28,15 +36,62 @@ function Highscores({ user }) {
   const [error, setError] = useState("");
 
   const classIconMap = {
-    berserker: {
-      ids: [1, 2, 3, 4, 5],
-      icon: BerserkerIcon,
+    dw: {
+      ids: [0, 1, 3, 7, 15],
+      icon: DwIcon,
     },
-    deadeye: {
-      ids: [6, 7, 8, 9, 10],
-      icon: DeadeyeIcon,
+    dk: {
+      ids: [16, 17, 19, 23, 31],
+      icon: DkIcon,
     },
-    // add the rest of your classes here...
+    elf: {
+      ids: [32, 33, 35, 39, 47],
+      icon: ElfIcon,
+    },
+    mg: {
+      ids: [48, 49, 51, 55, 63],
+      icon: MgIcon,
+    },
+    dl: {
+      ids: [64, 65, 67, 71, 79],
+      icon: DlIcon,
+    },
+    sum: {
+      ids: [80, 81, 83, 87, 95],
+      icon: SumIcon,
+    },
+    rf: {
+      ids: [96, 97, 99, 103, 111],
+      icon: RfIcon,
+    },
+    gl: {
+      ids: [112, 113, 115, 119, 127],
+      icon: GlIcon,
+    },
+    rw: {
+      ids: [128, 129, 131, 135, 143],
+      icon: RwIcon,
+    },
+    sl: {
+      ids: [144, 145, 147, 151, 159],
+      icon: SlIcon,
+    },
+    gc: {
+      ids: [160, 161, 163, 167, 175],
+      icon: GcIcon,
+    },
+    lw: {
+      ids: [176, 177, 179, 183, 191],
+      icon: LwIcon,
+    },
+    ma: {
+      ids: [192, 193, 195, 199, 207],
+      icon: MaIcon,
+    },
+    ik: {
+      ids: [208, 209, 211, 215, 223],
+      icon: IkIcon,
+    },
   };
 
   function getClassIcon(raceId) {
