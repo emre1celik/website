88c9d8329917c6230c72e9f5e815e37000a825a8
emre1celik <emre1celik@gmail.com>
2025-10-01 20:43:57 +0200

    X

--- src/pages/controlPanel/ControlPanel.js
+++ src/pages/controlPanel/ControlPanel.js
@@ -21,6 +21,21 @@ import {
   faUpLong,
   faArrowsRotate,
 } from "@fortawesome/free-solid-svg-icons";
+import DwIcon from "../../assets/images/classes/dw.png";
+import DkIcon from "../../assets/images/classes/dk.png";
+import ElfIcon from "../../assets/images/classes/elf.png";
+import MgIcon from "../../assets/images/classes/mg.png";
+import DlIcon from "../../assets/images/classes/dl.png";
+import SumIcon from "../../assets/images/classes/sum.png";
+import RfIcon from "../../assets/images/classes/rf.png";
+import GlIcon from "../../assets/images/classes/gl.png";
+import RwIcon from "../../assets/images/classes/rw.png";
+import SlIcon from "../../assets/images/classes/sl.png";
+import GcIcon from "../../assets/images/classes/gc.png";
+import LwIcon from "../../assets/images/classes/lw.png";
+import MaIcon from "../../assets/images/classes/ma.png";
+import IkIcon from "../../assets/images/classes/ik.png";
+import DefaultIcon from "../../assets/images/classes/default.png";
 
 function ControlPanel({ user }) {
   const [activeTab, setActiveTab] = useState("profile");
@@ -30,6 +45,49 @@ function ControlPanel({ user }) {
   const [loading, setLoading] = useState(true);
   const [characters, setCharacters] = useState([]);
   const [charsLoading, setCharsLoading] = useState(true);
+
+  const classNamesMap = {
+    dw: "Dark Wizard",
+    dk: "Dark Knight",
+    elf: "Elf",
+    mg: "Magic Gladiator",
+    dl: "Dark Lord",
+    sum: "Summoner",
+    rf: "Rage Fighter",
+    gl: "Grow Lancer",
+    rw: "Rune Wizard",
+    sl: "Slayer",
+    gc: "Gun Grusher",
+    lw: "Light Wizard",
+    ma: "Mage: Lemuria",
+    ik: "Illusion Knight",
+  };
+
+  const classIconMap = {
+    dw: { ids: [0, 1, 3, 7, 15], icon: DwIcon },
+    dk: { ids: [16, 17, 19, 23, 31], icon: DkIcon },
+    elf: { ids: [32, 33, 35, 39, 47], icon: ElfIcon },
+    mg: { ids: [48, 49, 51, 55, 63], icon: MgIcon },
+    dl: { ids: [64, 65, 67, 71, 79], icon: DlIcon },
+    sum: { ids: [80, 81, 83, 87, 95], icon: SumIcon },
+    rf: { ids: [96, 97, 99, 103, 111], icon: RfIcon },
+    gl: { ids: [112, 113, 115, 119, 127], icon: GlIcon },
+    rw: { ids: [128, 129, 131, 135, 143], icon: RwIcon },
+    sl: { ids: [144, 145, 147, 151, 159], icon: SlIcon },
+    gc: { ids: [160, 161, 163, 167, 175], icon: GcIcon },
+    lw: { ids: [176, 177, 179, 183, 191], icon: LwIcon },
+    ma: { ids: [192, 193, 195, 199, 207], icon: MaIcon },
+    ik: { ids: [208, 209, 211, 215, 223], icon: IkIcon },
+  };
+
+  function getClassInfo(raceId) {
+    for (const key in classIconMap) {
+      if (classIconMap[key].ids.includes(raceId)) {
+        return { icon: classIconMap[key].icon, key };
+      }
+    }
+    return { icon: DefaultIcon, key: "unknown" };
+  }
 
   useEffect(() => {
     const fetchCharacters = async () => {
@@ -301,58 +359,76 @@ function ControlPanel({ user }) {
         return (
           <div>
             <h3>Character Statistics</h3>
-            {characters.map((char, idx) => (
-              <div
-                key={idx}
-                style={{
-                  marginBottom: "1.5rem",
-                  padding: "1rem",
-                  border: "1px solid #555",
-                  borderRadius: "8px",
-                  background: "rgba(255,255,255,0.05)",
-                }}
-              >
-                <p>
-                  <strong>Name:</strong> {char.name}
-                </p>
-                <p>
-                  <strong>Level:</strong> {char.level}
-                </p>
-                <p>
-                  <strong>Resets:</strong> {char.reset}
-                </p>
-                <p>
-                  <strong>Race:</strong> {char.race}
-                </p>
-
-                {/* Action buttons */}
+            {characters.map((char, idx) => {
+              const classInfo = getClassInfo(char.race);
+              return (
                 <div
-                  style={{ marginTop: "1rem", display: "flex", gap: "0.5rem" }}
+                  key={idx}
+                  style={{
+                    marginBottom: "1.5rem",
+                    padding: "1rem",
+                    border: "1px solid #555",
+                    borderRadius: "8px",
+                    background: "rgba(255,255,255,0.05)",
+                  }}
                 >
-                  <GreenButton>
-                    <FontAwesomeIcon
-                      icon={faLocationCrosshairs}
-                      style={{ marginRight: "5px" }}
+                  <p>
+                    <strong>Name:</strong> {char.name}
+                  </p>
+                  <p>
+                    <strong>Level:</strong> {char.level}
+                  </p>
+                  <p>
+                    <strong>Resets:</strong> {char.reset}
+                  </p>
+                  <p>
+                    <strong>Race:</strong>{" "}
+                    <img
+                      src={classInfo.icon}
+                      alt={classInfo.key}
+                      style={{
+                        width: "24px",
+                        height: "24px",
+                        marginRight: "6px",
+                        verticalAlign: "middle",
+                      }}
                     />
-                    Unstuck
-                  </GreenButton>
-                  <GreenButton>
-                    <FontAwesomeIcon
-                      icon={faUpLong}
-                      style={{ marginRight: "5px" }}
-                    />
-                    Evolve
-                  </GreenButton>
-                  <GreenButton>
-                    <FontAwesomeIcon
-                      icon={faArrowsRotate}
-                      style={{ marginRight: "5px" }}
-                    />
-                    Grand Reset
-                  </GreenButton>
+                    {classNamesMap[classInfo.key] || "Unknown"}
+                  </p>
+
+                  {/* Action buttons */}
+                  <div
+                    style={{
+                      marginTop: "1rem",
+                      display: "flex",
+                      gap: "0.5rem",
+                    }}
+                  >
+                    <GreenButton>
+                      <FontAwesomeIcon
+                        icon={faLocationCrosshairs}
+                        style={{ marginRight: "5px" }}
+                      />
+                      Unstuck
+                    </GreenButton>
+                    <GreenButton>
+                      <FontAwesomeIcon
+                        icon={faUpLong}
+                        style={{ marginRight: "5px" }}
+                      />
+                      Evolve
+                    </GreenButton>
+                    <GreenButton>
+                      <FontAwesomeIcon
+                        icon={faArrowsRotate}
+                        style={{ marginRight: "5px" }}
+                      />
+                      Grand Reset
+                    </GreenButton>
+                  </div>
                 </div>
-              </div>
-            ))}
+              );
+            })}
           </div>
         );
 
