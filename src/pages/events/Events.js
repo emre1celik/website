d62f6df745e8812c27439856e59cfc278b6f8e07
emre1celik <emre1celik@gmail.com>
2025-10-07 22:03:13 +0200

    X

--- src/pages/events/Events.js
+++ src/pages/events/Events.js
@@ -245,7 +245,7 @@ function Events({ user }) {
                       <td>
                         {isNow ? (
                           <GlowingName rank={1}>
-                            {translate("now")}!
+                            {translate("events.now")}!
                           </GlowingName>
                         ) : (
                           formatCountdown(nextTime)
