cb2a9c9812dd520e5de5be48b99745c25462e424
Emre1Celik <e.celik@evasolutions.eu>
2025-10-10 12:10:21 +0200

    X

--- src/pages/control_panel/profile/ControlPanelProfile.js
+++ src/pages/control_panel/profile/ControlPanelProfile.js
@@ -1,9 +1,10 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
+import { faCartShopping, faSpinner } from "@fortawesome/free-solid-svg-icons";
+import { GreenButton } from "../ControlPanelStyles";
 
-export default function ControlPanelProfile({ profile, translate }) {
-  if (!profile) return <p>{translate("controlPanel.notLoad")}</p>;
+export default function ControlPanelProfile({ profile, translate, loading }) {
+  if (loading) return <p><FontAwesomeIcon icon={faSpinner} spin /> {translate("controlPanel.loading")}</p>;
+  if (!profile) return <p><FontAwesomeIcon icon={faSpinner} spin /> {translate("controlPanel.notLoad")}</p>;
 
   return (
     <div>
@@ -64,13 +65,40 @@ export default function ControlPanelProfile({ profile, translate }) {
               color: "#ccc",
             }}
           />
-          <button style={{ flex: 1, padding: "0.5rem", borderRadius: "5px" }}>
-            <FontAwesomeIcon icon={faCartShopping} /> {translate("controlPanel.profile.buy")}
-          </button>
+          <GreenButton style={{ flex: 1 }}>
+            <FontAwesomeIcon icon={faCartShopping} style={{ marginRight: "5px" }} /> {translate("controlPanel.profile.buy")}
+          </GreenButton>
         </div>
       </div>
 
-      {/* Add remaining profile fields similarly */}
+      <div style={{ marginBottom: "1rem" }}> 
+        <label>{translate("controlPanel.profile.goblinPoints")}</label> 
+        <br /> 
+        <div style={{ display: "flex", gap: "0.5rem", marginTop: "0.3rem" }} > 
+          <input 
+            type="number" 
+            value={profile.goblin_points.toLocaleString()} 
+            disabled 
+            style={{ 
+              flex: 2, 
+              padding: "0.5rem", 
+              backgroundColor: "rgba(255,255,255,0.1)", 
+              border: "1px solid #555", 
+              borderRadius: "5px", 
+              color: "#ccc", 
+            }} 
+          /> 
+          <GreenButton style={{ flex: 1 }}> 
+            <FontAwesomeIcon icon={faCartShopping} style={{ marginRight: "5px" }} /> {translate("controlPanel.profile.buy")} 
+          </GreenButton> 
+        </div> 
+      </div> 
+      
+      <div style={{ marginBottom: "1rem" }}> 
+        <label>{translate("controlPanel.profile.lastLogin")}</label> 
+        <br /> 
+        <input type="text" value={profile.last_login} disabled style={{ width: "100%", padding: "0.5rem", marginTop: "0.3rem", backgroundColor: "rgba(255,255,255,0.1)", border: "1px solid #555", borderRadius: "5px", color: "#ccc", }} /> 
+      </div> 
     </div>
   );
 }
--- src/pages/control_panel/settings/ControlPanelSettings.js
+++ src/pages/control_panel/settings/ControlPanelSettings.js
@@ -1,4 +1,3 @@
-import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
 
