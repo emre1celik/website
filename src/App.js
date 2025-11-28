fef449038aba967ab35cd5901d9377cb9cb5c693
emre1celik <emre1celik@gmail.com>
2025-11-28 22:08:37 +0100

    X

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/landing/Landing";
 import Register from "./pages/register/Register";
